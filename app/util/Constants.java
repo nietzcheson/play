730e3a6fb2450dee5dfb336dbc449e82920eb729
Karla Tirado <ktirado@sunset.com.mx>
2016-04-11 10:42:25 -0500

    Cambiando de versión

--- app/util/Constants.java
+++ app/util/Constants.java
@@ -5,11 +5,11 @@ package util;
  */
 public final class Constants {
 //    public static final String API = "http://localhost:9010/v1";
-    public static final String API = "http://10.194.21.232:9010/v1";
-//    public static final String API = "http://acuarius.it.sunset.com.mx:1010/v1";
+//    public static final String API = "http://10.194.21.232:9010/v1";
+    public static final String API = "http://acuarius.it.sunset.com.mx:1010/v1";
     public static final String URL_ECERT = "http://bpo.it.sunset.com.mx:8080";
     public static final String CertLink = "http://bpo.it.sunset.com.mx:9010";
     public static final String user = "sistemas";
     public static final String password = "sys73xrv";
-    public static final String version="1.6.1";
+    public static final String version="1.7.0";
 }
