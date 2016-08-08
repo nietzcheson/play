e95c3af30de44acf04869bef14e5317069c4c3df
Cristian Angulo Nova <cristianangulonova@gmail.com>
2016-08-08 11:00:59 -0500

    Constantes de APIS para productivo

--- app/util/Constants.java
+++ app/util/Constants.java
@@ -5,8 +5,8 @@ package util;
  */
 public final class Constants {
 
-    public static final String API_Bulkbank = "http://127.0.0.1:1010/v1";
-    public static final String API = "http://127.0.0.1:9010/v1";
+    public static final String API_Bulkbank = "http://10.194.17.173:1010/v1";
+    public static final String API = "http://acuarius.it.sunset.com.mx:1010/v1";
     public static final String URL_ECERT = "http://bpo.it.sunset.com.mx:8080";
     public static final String CertLink = "http://bpo.it.sunset.com.mx:9010";
     public static final String user = "sistemas";
